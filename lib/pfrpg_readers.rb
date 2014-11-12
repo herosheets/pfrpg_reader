b490d9769b297198a6a8eb2f06cb77f612588922
Jordan OMara <jomara@redhat.com>
2014-11-12 09:57:44 -0500

    Moving module definition

--- lib/pfrpg_readers.rb
+++ lib/pfrpg_readers.rb
@@ -1,8 +1,8 @@
+module PfrpgReaders
+end
+
 require 'pfrpg_readers/defensible'
 
 Dir[File.expand_path(File.join(File.dirname(File.absolute_path(__FILE__)), 'pfrpg_readers/')) + "/**/*.rb"].each do |file|
   require file
-end
-
-module PfrpgReaders
 end
